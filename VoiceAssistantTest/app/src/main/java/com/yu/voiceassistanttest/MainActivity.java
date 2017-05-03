b29fbd9f16b6196815728e83b2e4be3e568d67b1
OhOHOh <15221589025@163.com>
2017-05-03 18:25:44 +0800

    隐藏任务栏
    隐藏任务栏

--- VoiceAssistantTest/app/src/main/java/com/yu/voiceassistanttest/MainActivity.java
+++ VoiceAssistantTest/app/src/main/java/com/yu/voiceassistanttest/MainActivity.java
@@ -14,6 +14,7 @@ import android.text.TextUtils;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -62,6 +63,11 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        //隐藏标题栏
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
